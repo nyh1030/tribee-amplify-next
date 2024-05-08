d13f4d7810e5b72d6eed377f9ee0cbe474fcadb0
yonghwan nam <nyhpig@gmail.com>
2024-05-09 01:20:36 +0900

    feat: 환경별 oauth 승인 url 변경 v3
    초기값 추가

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -3,8 +3,8 @@ import {defineAuth, secret} from '@aws-amplify/backend';
 const environment = process.env.NODE_ENV; // 환경 변수에서 환경 설정 읽기
 
 // 초기 배열 값을 설정하여 타입 에러 방지
-let callbackUrls: string[] = [];
-let logoutUrls: string[] = [];
+let callbackUrls: string[] = ['http://localhost:3000'];
+let logoutUrls: string[] = ['http://localhost:3000'];
 
 if (environment === 'production') {
   callbackUrls = ['https://main.d1nwj521dwh1h1.amplifyapp.com'];
@@ -23,8 +23,8 @@ export const auth = defineAuth({
         clientSecret: secret('GOOGLE_CLIENT_SECRET'),
         scopes: ['email']
       },
-      callbackUrls,  // 사용자 인증 성공 후 리디렉션될 주소를 배열로 지정
-      logoutUrls,  // 로그아웃 후 리디렉션될 주소를 배열로 지정
+      callbackUrls : callbackUrls,  // 사용자 인증 성공 후 리디렉션될 주소를 배열로 지정
+      logoutUrls : logoutUrls,  // 로그아웃 후 리디렉션될 주소를 배열로 지정
     }
   }
 });
